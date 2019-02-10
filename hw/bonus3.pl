e21d8515ae56a9b71bcbab2725d95bdfa5436b9f
TheGreatNectarine <margal.nik@gmail.com>
2019-02-10 22:33:16 +0200

    sass)

---
+++ hw/bonus3.pl
@@ -0,0 +1,9 @@
+solve :- solve(10, 6, RES), writeln(RES).
+solve(N, M, RES):- DIFF is N - M, fac(N, F1), fac(DIFF, F2), RES is F1 / F2.
+
+fac(0,1).
+fac(N,F) :- N > 0, N1 is N - 1, fac(N1, F1), F is N * F1.
+
+
+:- write("Total number of combinations: ").
+:- solve.
